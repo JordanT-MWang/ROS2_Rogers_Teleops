242be6a52854f236bd545969ff4acc8a18154edd
Omar Laptop <jordan80wang@gmail.com>
2024-11-05 14:36:13 -0500

    Connection setups scripts

---
+++ Spring_Balanced_Arm_Code/Comunication_setups/setup_ethernet.bash
@@ -0,0 +1,4 @@
+source /opt/ros/humble/setup.bash
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+export FASTRTPS_DEFAULT_PROFILES_FILE=${DIR}/fastdds_ethernet.xml
